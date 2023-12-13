9cdbc63c1dee15c2c49ec004e3a98290fd07a53f
dos4iiiik <dos4iiiik@yandex.ru>
2023-12-13 13:21:28 +0700

    first

---
+++ ProgrammING/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammING
+{
+    public class Program
+    {
+        public static int SumD(int num)  // сумма всех цифр в числе
+        {
+            int sm = 0;
+            while (num > 0)
+            {
+                sm += num % 10;
+                num /= 10;
+            }
+            return sm;
+        }
+
+        public static int CountD(int num) // количество цифр в числе
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                count += 1;
+                num /= 10;
+            }
+            return count;
+        }
+
+        public static int MaxD(int num) //максимальная цифра
+        {
+            int mx = -1;
+            while (num > 0)
+            {
+                if (num % 10 > mx) { mx = num % 10; }               
+                num /= 10;
+            }
+            return mx;
+        }
+
+        static void Main(string[] args)
+        {
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Сумма всех чисел введенного числа: " + SumD(number));
+            Console.WriteLine("Количество цифр в введенном числе: " + CountD(number));
+            Console.WriteLine("Максимальная цифра введенного числа: " + MaxD(number));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sum/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProgrammING;
+
+namespace Sum
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            int sum;           
+            sum = Program.SumD(123);
+            Assert.AreEqual(6, sum);
+        }
+
+        [TestMethod]
+        public void TestCountD()
+        {
+            int count;
+            count = Program.CountD(123);
+            Assert.AreEqual(3, count);
+        }
+
+        [TestMethod]
+        public void TestMaxD()
+        {
+            int maxD;
+            maxD = Program.MaxD(123);
+            Assert.AreEqual(3, maxD);
+        }
+    }
+}
